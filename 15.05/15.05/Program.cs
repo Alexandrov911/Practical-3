daa212128fe137c06a19ca8ab16df529210be5cb
Alexandrov911 <sashok17062002@mail.ru>
2021-05-15 13:00:36 +0300

    Практическая 3

---
+++ 15.05/15.05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите трехзначное число");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int a = k % 10;
+            int b = k / 100;
+
+            if (a == b)
+            {
+                Console.WriteLine("палиндром");
+            }
+            else
+            {
+                Console.WriteLine("не палиндром");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 15.05/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите x");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y = 0;
+            if (x>0)
+            {
+                y=(Math.Pow(Math.Sin(x),2));
+
+            }
+            else  
+            {
+                y= 1 - 2 * Math.Sin(x*x);
+            }
+            Console.WriteLine("Ответ:"+ y);
+            Console.ReadKey();
+
+
+        }
+    }
+}
